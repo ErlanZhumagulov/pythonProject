a45c128325adcd5448b98d58dc4c056e4b854c73
Ерлан <erlanrulit@mail.ru>
2023-07-13 23:36:13 +0400

    cde

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
@@ -9,23 +10,7 @@ options = webdriver.ChromeOptions()
 
 options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
 options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--disable-features=SameSiteByDefaultCookies")
-options.add_argument("--disable-features=CrossSiteDocumentBlockingIfIsolating")
-options.add_argument("--disable-features=CrossSiteDocumentBlockingAlways")
-options.add_argument("--disable-features=SameSiteDefaultChecksMethodRigorously")
 options.add_argument("--headless")  # Опционально: запуск в безголовом режиме
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-browser-side-navigation")
-options.add_argument("--disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-popup-blocking")
-options.add_argument("--ignore-certificate-errors")
-options.add_argument("--ignore-ssl-errors")
-options.add_argument("cookie=domain=.mvideo.ru; SameSite=None; Secure")  # Установка куки с доменом .mvideo.ru
-
-
 
 s = Service(executable_path="/path/chromedriver")
 driver = webdriver.Chrome(service=s, options=options)
@@ -38,99 +23,86 @@ try:
     pagesNumbers = driver.find_elements(By.CLASS_NAME, "page-item")
     pagesCount = int(pagesNumbers[-2].find_element(By.TAG_NAME, 'a').text)
     print("Количество страниц " + str(pagesCount))
+
+    dict_prods = {}
+
     n = 1
     while n <= pagesCount:
-        #driver.get("https://www.mvideo.ru/televizory-i-cifrovoe-tv-1/televizory-65/f/tolko-v-nalichii=da?from=under_search&page=" + str(n))
-        cookies = driver.get_cookies()
-        #print(cookies)
-        cookies_to_extract = [
-            '__hash_',
-            '__lhash_',
-            'MVID_ALFA_PODELI_NEW',
-            'MVID_BLACK_FRIDAY_ENABLED',
-            'MVID_CASCADE_CMN',
-            'MVID_CATALOG_STATE',
-            'MVID_CHECKOUT_STORE_SORTING',
-            'MVID_CITY_ID',
-            'MVID_CREDIT_SERVICES',
-            'MVID_CRITICAL_GTM_INIT_DELAY',
-            'MVID_FILTER_CODES',
-            'MVID_FILTER_TOOLTIP',
-            'MVID_FLOCKTORY_ON',
-            'MVID_GEOLOCATION_NEEDED',
-            'MVID_GIFT_KIT',
-            'MVID_GTM_ENABLED',
-            'MVID_INTERVAL_DELIVERY',
-            'MVID_IS_NEW_BR_WIDGET',
-            'MVID_KLADR_ID',
-            'MVID_LAYOUT_TYPE',
-            'MVID_LP_SOLD_VARIANTS',
-            'MVID_MCLICK',
-            'MVID_MINDBOX_DYNAMICALLY',
-            'MVID_MINI_PDP',
-            'MVID_NEW_ACCESSORY',
-            'MVID_NEW_LK_CHECK_CAPTCHA',
-            'MVID_NEW_LK_OTP_TIMER',
-            'MVID_NEW_MBONUS_BLOCK',
-            'MVID_PODELI_PDP',
-            'MVID_PROMO_CATALOG_ON',
-            'MVID_REGION_ID',
-            'MVID_REGION_SHOP',
-            'MVID_SERVICES',
-            'MVID_SP',
-            'MVID_TIMEZONE_OFFSET',
-            'MVID_TYP_CHAT',
-            'MVID_WEB_SBP',
-            'SENTRY_ERRORS_RATE',
-            'SENTRY_TRANSACTIONS_RATE',
-            'MVID_ENVCLOUD',
-            'gsscgib-w-mvideo',
-            'gsscgib-w-mvideo',
-            '_ga_CFMZTSS5FM',
-            '_sp_ses.d61c',
-            '_sp_id.d61c',
-            '_ga_BNX5WPP3YK',
-            '_ym_uid',
-            '_ym_d',
-            '_ga',
-            '_gid',
-            '_dc_gtm_UA-1873769-1',
-            '_ym_isad',
-            '_dc_gtm_UA-1873769-37',
-            'tmr_lvid',
-            'tmr_lvidTS',
-            'tmr_detect',
-            'uxs_uid',
-            'advcake_track_id',
-            'advcake_session_id',
-            'flocktory-uuid',
-            'flacktory',
-            'BIGipServeratg-ps-prod_tcp80',
-            'bIPs',
-            'afUserId',
-            'AF_SYNC',
-            'fgsscgib-w-mvideo',
-            'fgsscgib-w-mvideo',
-            'gssc218',
-            'cfidsgib-w-mvideo'
-        ]
-        cookies_dict = {}
-
-        for cookie_name in cookies_to_extract:
-            found_cookie = next((cookie for cookie in cookies if cookie['name'] == cookie_name), None)
-            if found_cookie:
-                cookies_dict[cookie_name] = found_cookie['value']
-        #     else:
-        #          print(f"Куки по имени '{cookie_name}' не найден")
-        #
-        # print(cookies_dict)
-
+        driver.get("https://www.mvideo.ru/televizory-i-cifrovoe-tv-1/televizory-65/f/tolko-v-nalichii=da?from=under_search&page=" + str(n))
 
         time.sleep(2)
-        Parser2.get_data(n, cookies_dict)
+
+        page_height = driver.execute_script("return document.body.scrollHeight")
+        print(page_height)
+
+        item_count_buf = 0
+        # Получение высоты страницы
+
+        last_page_stopper = 0
+        while True:
+            try:
+                button = driver.find_element(By.CLASS_NAME, "mobile-notification__button")
+                # Нажатие на кнопку
+                button.click()
+            except:
+                pass
+
+            # Прокрутка на 100 пикселей вверх
+            driver.execute_script("window.scrollBy(0, +300);")
+
+            # Пауза для прогрузки элементов
+            time.sleep(0.2)
+
+
+
+            items = driver.find_elements(By.TAG_NAME, 'mvid-plp-product-card')
+            if len(items) == 24:
+                break
+
+            last_page_stopper = last_page_stopper + 1
+            if n == pagesCount and last_page_stopper == 24:
+                break
+
+        items = driver.find_elements(By.TAG_NAME, 'mvid-plp-product-card')
+        prices = driver.find_elements(By.CLASS_NAME, 'price__main-value')
+        names = driver.find_elements(By.CLASS_NAME, 'product-title__text')
+        price_array = [price.text for price in prices]
+        name_array = [name.text for name in names]
+        for i in range(len(name_array)):
+            name_array[i] = name_array[i].replace('"', '').replace("'", '')
+        print("Ниже цены")
+        print(name_array)
+        print(price_array)
+        print(len(items))
+        print(len(price_array))
+        print(len(name_array))
+
+        for i in range(len(items)):
+            notification_element = items[i].find_elements(By.CLASS_NAME, 'product-notification_black')
+            if notification_element:
+                print("Элемент с классом 'product-notification_black' найден")
+                price_array.insert(i, 'Нет в наличии')
+
+
+            dict_prods[name_array[i]] = price_array[i]
+
+        # Вывод массива значений содержимого
+
+
+
+        #print(items)
 
         print('Цикл выполнился', n, 'раз(а)')
         n = n + 1
+
+    filename = 'parse_result.csv'
+
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Наименование', 'Цена'])
+
+        for item, price in dict_prods.items():
+            writer.writerow([item, price])
 
 
 except Exception as ex:
