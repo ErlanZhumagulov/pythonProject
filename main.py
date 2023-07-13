fe50a2d47840ae8fd58fb7ad3a54cd8b507d032a
Ерлан <erlanrulit@mail.ru>
2023-07-13 14:52:58 +0400

    cde

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from selenium.webdriver.common.by import By
 options = webdriver.ChromeOptions()
 
 
-options = webdriver.ChromeOptions()
-
 options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--disable-features=SameSiteByDefaultCookies")
@@ -25,10 +23,6 @@ options.add_argument("--disable-extensions")
 options.add_argument("--disable-popup-blocking")
 options.add_argument("--ignore-certificate-errors")
 options.add_argument("--ignore-ssl-errors")
-
-# Установка параметров куки
-options.add_argument("user-data-dir=/path/to/your/profile")  # Путь к вашему профилю Chrome
-options.add_argument("profile-directory=ProfileName")  # Имя профиля Chrome
 options.add_argument("cookie=domain=.mvideo.ru; SameSite=None; Secure")  # Установка куки с доменом .mvideo.ru
 
 
@@ -50,21 +44,75 @@ try:
         cookies = driver.get_cookies()
         #print(cookies)
         cookies_to_extract = [
-            'MVID_ALFA_PODELI_NEW', 'MVID_BLACK_FRIDAY_ENABLED', 'MVID_CATALOG_STATE', 'MVID_CHECKOUT_STORE_SORTING',
-            'MVID_CITY_ID', 'MVID_CREDIT_SERVICES', 'MVID_CRITICAL_GTM_INIT_DELAY', 'MVID_FILTER_CODES',
-            'MVID_FILTER_TOOLTIP', 'MVID_FLOCKTORY_ON', 'MVID_GEOLOCATION_NEEDED', 'MVID_GIFT_KIT', 'MVID_GTM_ENABLED',
-            'MVID_INTERVAL_DELIVERY', 'MVID_IS_NEW_BR_WIDGET', 'MVID_KLADR_ID', 'MVID_LAYOUT_TYPE',
-            'MVID_LP_SOLD_VARIANTS', 'MVID_MCLICK', 'MVID_MINDBOX_DYNAMICALLY', 'MVID_MINI_PDP', 'MVID_NEW_ACCESSORY',
-            'MVID_NEW_LK_CHECK_CAPTCHA', 'MVID_NEW_LK_OTP_TIMER', 'MVID_NEW_MBONUS_BLOCK', 'MVID_PROMO_CATALOG_ON',
-            'MVID_REGION_ID', 'MVID_REGION_SHOP', 'MVID_SP', 'MVID_TIMEZONE_OFFSET', 'MVID_TYP_CHAT', 'MVID_WEB_SBP',
-            'SENTRY_ERRORS_RATE', 'SENTRY_TRANSACTIONS_RATE', '_ym_uid', '_ym_d', 'tmr_lvid', 'tmr_lvidTS',
-            'advcake_session_id', 'afUserId', 'uxs_uid', 'flocktory-uuid', '_lhash', 'MVID_PODELI_PDP', 'MVID_SERVICES',
-            '_gid', 'AF_SYNC', 'partnerSrc', '__cpatrack', '__sourceid', '__allsource', 'advcake_track_id',
-            'advcake_track_url', 'advcake_utm_partner', 'advcake_utm_webmaster', 'advcake_click_id', 'MVID_ENVCLOUD',
-            '__SourceTracker', 'admitad_deduplication_cookie', 'SMSError', 'authError', 'BIGipServericerock-prod',
-            'bIPs', 'mindboxDeviceUUID', 'directCrm-session', '_js_p', '_jhash', '_jua', '_sp_ses.d61c', '_sp_id.d61c',
-            '_ym_isad', '_ga', 'tmr_detect', 'gsscgib-w-mvideo', 'fgsscgib-w-mvideo', '_hash', 'gssc218',
-            'cfidsgib-w-mvideo', '_ga_CFMZTSS5FM', '_ga_BNX5WPP3YK',
+            '__hash_',
+            '__lhash_',
+            'MVID_ALFA_PODELI_NEW',
+            'MVID_BLACK_FRIDAY_ENABLED',
+            'MVID_CASCADE_CMN',
+            'MVID_CATALOG_STATE',
+            'MVID_CHECKOUT_STORE_SORTING',
+            'MVID_CITY_ID',
+            'MVID_CREDIT_SERVICES',
+            'MVID_CRITICAL_GTM_INIT_DELAY',
+            'MVID_FILTER_CODES',
+            'MVID_FILTER_TOOLTIP',
+            'MVID_FLOCKTORY_ON',
+            'MVID_GEOLOCATION_NEEDED',
+            'MVID_GIFT_KIT',
+            'MVID_GTM_ENABLED',
+            'MVID_INTERVAL_DELIVERY',
+            'MVID_IS_NEW_BR_WIDGET',
+            'MVID_KLADR_ID',
+            'MVID_LAYOUT_TYPE',
+            'MVID_LP_SOLD_VARIANTS',
+            'MVID_MCLICK',
+            'MVID_MINDBOX_DYNAMICALLY',
+            'MVID_MINI_PDP',
+            'MVID_NEW_ACCESSORY',
+            'MVID_NEW_LK_CHECK_CAPTCHA',
+            'MVID_NEW_LK_OTP_TIMER',
+            'MVID_NEW_MBONUS_BLOCK',
+            'MVID_PODELI_PDP',
+            'MVID_PROMO_CATALOG_ON',
+            'MVID_REGION_ID',
+            'MVID_REGION_SHOP',
+            'MVID_SERVICES',
+            'MVID_SP',
+            'MVID_TIMEZONE_OFFSET',
+            'MVID_TYP_CHAT',
+            'MVID_WEB_SBP',
+            'SENTRY_ERRORS_RATE',
+            'SENTRY_TRANSACTIONS_RATE',
+            'MVID_ENVCLOUD',
+            'gsscgib-w-mvideo',
+            'gsscgib-w-mvideo',
+            '_ga_CFMZTSS5FM',
+            '_sp_ses.d61c',
+            '_sp_id.d61c',
+            '_ga_BNX5WPP3YK',
+            '_ym_uid',
+            '_ym_d',
+            '_ga',
+            '_gid',
+            '_dc_gtm_UA-1873769-1',
+            '_ym_isad',
+            '_dc_gtm_UA-1873769-37',
+            'tmr_lvid',
+            'tmr_lvidTS',
+            'tmr_detect',
+            'uxs_uid',
+            'advcake_track_id',
+            'advcake_session_id',
+            'flocktory-uuid',
+            'flacktory',
+            'BIGipServeratg-ps-prod_tcp80',
+            'bIPs',
+            'afUserId',
+            'AF_SYNC',
+            'fgsscgib-w-mvideo',
+            'fgsscgib-w-mvideo',
+            'gssc218',
+            'cfidsgib-w-mvideo'
         ]
         cookies_dict = {}
 
